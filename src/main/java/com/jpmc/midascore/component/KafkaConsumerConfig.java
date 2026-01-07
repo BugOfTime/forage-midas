8a20ba994cbb8644958798d51ff89dfd7997973c
Yulin Wang <78449603+BugOfTime@users.noreply.github.com>
2026-01-07 16:55:13 +0000

    Implement Kafka consumer and transaction listener; update config

---
+++ src/main/java/com/jpmc/midascore/component/KafkaConsumerConfig.java
@@ -0,0 +1,41 @@
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConsumerFactory<String, Transaction> transactionConsumerFactory() {
+        JsonDeserializer<Transaction> jsonDeserializer = new JsonDeserializer<>(Transaction.class);
+
+        jsonDeserializer.addTrustedPackages("com.jpmc.midascore.foundation");
+
+        Map<String, Object> props = new HashMap<>();
+
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "midas-core");
+
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), jsonDeserializer);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Transaction> transactionKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Transaction> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(transactionConsumerFactory());
+        return factory;
+    }
+}
